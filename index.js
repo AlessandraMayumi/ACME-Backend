03156de6f2dbf18e12e8a7b62362f728ce4727f0
AlessandraMayumi <alessandra_mms@hotmail.com>
2020-02-08 19:35:58 -0300

    body-parser to enable read req.body

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-const express = require('express')
-const path = require('path')
-const mongoose = require('mongoose')
-const clientRoutes = require('./src/routes/crm/clientRoutes')
-const serviceRoutes = require('./src/routes/crm/serviceRoutes')
-const paidRoutes = require('./src/routes/financial/paidRoutes')
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const clientRoutes = require('./src/routes/crm/clientRoutes');
+const serviceRoutes = require('./src/routes/crm/serviceRoutes');
+const paidRoutes = require('./src/routes/financial/paidRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-//app.get('/', (req, res) => res.render('pages/index'))
+// mongoose connection
+const uristring =
+    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/ACMEdb';
+mongoose.Promise = global.Promise;
+mongoose.connect(uristring, {   
+    useNewUrlParser: true,
+    useUnifiedTopology: true},
+    function (err, res) {
+        if (err) {
+        console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+        }
+        console.log ('Succeeded connected to: ' + uristring);
+      },
+    );
+// body-parser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//app.get('/', (req, res) => res.render('pages/index'));
 app.get('/', (req,res) =>
     res.send(`Node and Express Server running on port ${PORT}`)
 );
@@ -23,10 +44,3 @@ app.get('/', (req,res) =>
 app.use('/', clientRoutes);
 app.use('/', serviceRoutes);
 app.use('/', paidRoutes);
-
-// mongoose connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/ACMEdb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
