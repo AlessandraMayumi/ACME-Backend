d1146308a8f8db3e4f7e7c0a63ae603cc0b205b5
AlessandraMayumi <alessandra_mms@hotmail.com>
2020-02-08 18:13:48 -0300

    Add cool face API

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+const cool = require('cool-ascii-faces')
 const express = require('express')
+const path = require('path')
 const mongoose = require('mongoose')
 const clientRoutes = require('./src/routes/crm/clientRoutes')
 const serviceRoutes = require('./src/routes/crm/serviceRoutes')
 const paidRoutes = require('./src/routes/financial/paidRoutes')
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/cool', (req, res) => res.send(cool()))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 // mongoose connection
 mongoose.Promise = global.Promise;
@@ -17,10 +26,6 @@ mongoose.connect('mongodb://localhost/ACMEdb', {
 app.use('/', clientRoutes);
 app.use('/', serviceRoutes);
 app.use('/', paidRoutes);
-
-// serving static files
-app.use(express.static('public'));
-//app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req,res) =>
     res.send(`Node and Express Server running on port ${PORT}`)
