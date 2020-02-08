84e3ff5650c1781f08b2f0423330ea96247880d3
AlessandraMayumi <alessandra_mms@hotmail.com>
2020-02-08 20:25:32 -0300

    heroku: add mongolab

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 // mongoose connection
 const uristring =
-    process.env.MONGOLAB_URI ||
-    process.env.MONGOHQ_URL ||
+    process.env.MONGOLAB_TEAL_URI ||
     'mongodb://localhost/ACMEdb';
 mongoose.Promise = global.Promise;
 mongoose.connect(uristring, {   
@@ -29,7 +28,9 @@ mongoose.connect(uristring, {
         if (err) {
         console.log ('ERROR connecting to: ' + uristring + '. ' + err);
         }
-        console.log ('Succeeded connected to: ' + uristring);
+        else{
+            console.log ('Succeeded connected to: ' + uristring);
+        }
       },
     );
 // body-parser setup
@@ -40,10 +41,18 @@ app.use(bodyParser.json());
 app.get('/', (req,res) =>
     res.send(`Node and Express Server running on port ${PORT}`)
 );
-app.get('/teste', (req,res) =>
-    res.send(`teste ${PORT}`)
-);
 
 app.use('/', clientRoutes);
 app.use('/', serviceRoutes);
 app.use('/', paidRoutes);
+
+
+/*
+MONGODB_URI
+mongodb://heroku_jlrvxts9:p0h4fu2t42gl2rru73806fsslv@ds047387.mlab.com:47387/heroku_jlrvxts9
+
+To connect using the mongo shell:
+mongo ds061731.mlab.com:61731/heroku_vc84sm2k -u <dbuser> -p <dbpassword>
+To connect using a driver via the standard MongoDB URI (what's this?):
+mongodb://<dbuser>:<dbpassword>@ds061731.mlab.com:61731/heroku_vc84sm2k
+*/
